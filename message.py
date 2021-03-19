a66db69e3968a87a3b208b3ca5d7754de8695655
Santiago valencia <sanvalenciaarango@gmail.com>
2021-03-19 00:18:46 -0500

    add message class

---
+++ message.py
@@ -0,0 +1,20 @@
+class message:
+    def __init__(self, namei,named, data):
+        self.namei = namei
+        self.named = named
+        self.data = data
+    
+    
+
+    def get_namei(self):
+        return self.namei
+
+
+    def get_named(self):
+        return self.named
+
+
+
+    def get_data(self):
+
+        return self.data   
